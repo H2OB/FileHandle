a89ae4a0bee8ffbbdca6ef25fffd13f32a924702
North <yangwork@163.com>
2019-05-27 18:23:11 +0800

    写文件

--- FileDownLoad/ViewController.m
+++ FileDownLoad/ViewController.m
@@ -8,6 +8,12 @@
 
 #import "ViewController.h"
 #import <AFNetworking.h>
+
+#define CompletionData @"completionData" //最终数据
+#define ResumeData @"resumeData" //临时数据
+
+typedef void(^TaskCompletionBlock)(void);
+
 @interface ViewController ()
 {
     
@@ -22,6 +28,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(taskDidCompleteNotification:) name:AFNetworkingTaskDidCompleteNotification object:nil];
+    
     
     
 }
@@ -34,60 +42,145 @@
     NSURL *URL = [NSURL URLWithString:@"http://tb-video.bdstatic.com/videocp/16514218_b3883a9f1e041a181bda58804e0a5192.mp4"];
     //默认配置
   
-    if(!configuration){
-        
-        configuration = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:@"com.newhope.one"];
-        configuration.HTTPMaximumConnectionsPerHost = 10;
-    }
-    
-    
+    configuration = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:@"com.newhope.one"];
+    configuration.HTTPMaximumConnectionsPerHost = 10;
+
     //AFN3.0+基于封住URLSession的句柄
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
 //    manager.operationQueue = dispatch_queue_create(@"com.newhope.one", 0);
     //请求
     NSURLRequest *request = [NSURLRequest requestWithURL:URL];
     
-    //下载Task操作
-    _downloadTask = [manager downloadTaskWithRequest:request progress:^(NSProgress * _Nonnull downloadProgress) {
+    NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSString *filePath = [documentPath stringByAppendingPathComponent:ResumeData];
+    filePath = [filePath stringByAppendingPathComponent:request.URL.lastPathComponent];
+    
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    
+    if(data.length > 0){
         
-        // @property int64_t totalUnitCount;     需要下载文件的总大小
-        // @property int64_t completedUnitCount; 当前已经下载的大小
         
-        // 回到主队列刷新UI
-        dispatch_async(dispatch_get_main_queue(), ^{
-            // 设置进度条的百分比
-            self->progressView.progress = (1.0 * downloadProgress.completedUnitCount / downloadProgress.totalUnitCount);
+        _downloadTask = [manager downloadTaskWithResumeData:data progress:^(NSProgress * _Nonnull downloadProgress) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // 设置进度条的百分比
+                self->progressView.progress = (1.0 * downloadProgress.completedUnitCount / downloadProgress.totalUnitCount);
+                NSLog(@"%.2f",self->progressView.progress);
+            });
             
-            NSLog(@"%.2f",self->progressView.progress);
-        });
-        
-    } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
-        
-        NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-        
-        NSString *filePath = [documentPath stringByAppendingPathComponent:URL.lastPathComponent];
-        
-        return [NSURL fileURLWithPath:filePath];
-        
-    } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
-        
-        
-        
-        
-        
-        if([[NSFileManager defaultManager] fileExistsAtPath:filePath.path]) {
+        } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
             
-            NSLog(@"%@",filePath.path);
+            NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+            NSString *filePath = [documentPath stringByAppendingPathComponent:CompletionData];
+            filePath = [filePath stringByAppendingPathComponent:URL.lastPathComponent];
+            
+            return [NSURL fileURLWithPath:filePath];
+            
+        } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
+            
+            if([[NSFileManager defaultManager] fileExistsAtPath:filePath.path]) {
+                
+                [manager.session finishTasksAndInvalidate];
+                //            [manager.session invalidateAndCancel];
+                NSLog(@"%@",filePath.path);
+                
+                [[NSFileManager defaultManager] removeItemAtPath:[filePath.path stringByReplacingOccurrencesOfString:CompletionData withString:ResumeData] error:nil];
+                
+                
+                
+                
+            }else{
+                
+                
+            }
+        }];
+        
+       
+        
+    }else{
+        
+        
+        //下载Task操作
+        _downloadTask = [manager downloadTaskWithRequest:request progress:^(NSProgress * _Nonnull downloadProgress) {
+            
+            // @property int64_t totalUnitCount;     需要下载文件的总大小
+            // @property int64_t completedUnitCount; 当前已经下载的大小
+            
+            // 回到主队列刷新UI
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // 设置进度条的百分比
+                self->progressView.progress = (1.0 * downloadProgress.completedUnitCount / downloadProgress.totalUnitCount);
+                
+                NSLog(@"%.2f",self->progressView.progress);
+            });
+            
+        } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
+            
+            NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+            NSString *filePath = [documentPath stringByAppendingPathComponent:CompletionData];
+            filePath = [filePath stringByAppendingPathComponent:URL.lastPathComponent];
+            
+            return [NSURL fileURLWithPath:filePath];
+            
+        } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
+            
+            
+            
+            
+            if([[NSFileManager defaultManager] fileExistsAtPath:filePath.path]) {
+                
+                [manager.session finishTasksAndInvalidate];
+                //            [manager.session invalidateAndCancel];
+//                NSLog(@"%@",filePath.path);
+
+                [[NSFileManager defaultManager] removeItemAtPath:[filePath.path stringByReplacingOccurrencesOfString:CompletionData withString:ResumeData] error:nil];
+
+            }else{
+                
+                
+            }
+            
+        }];
+    }
+
+    
+    [_downloadTask resume];
+}
+
+- (void)taskDidCompleteNotification:(NSNotification *)notification{
+    
+    if ([notification.object isKindOfClass:[ NSURLSessionDownloadTask class]]) {
+        NSURLSessionDownloadTask *task = notification.object;
+       
+        NSError *error  = [notification.userInfo objectForKey:AFNetworkingTaskDidCompleteErrorKey] ;
+        if (error) {
+            
+            NSData *resumeData = [error.userInfo objectForKey:@"NSURLSessionDownloadTaskResumeData"];
+            //这个是因为 用户比如强退程序之后 ,再次进来的时候 存进去这个继续的data  需要用户去刷新列表
+            
+            NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+            NSString *filePath = [documentPath stringByAppendingPathComponent:ResumeData];
+            filePath = [filePath stringByAppendingPathComponent:task.currentRequest.URL.lastPathComponent];
+             NSError *error = nil;
+            
+            if(![[NSFileManager defaultManager] fileExistsAtPath:filePath]){
+                
+                [[NSFileManager defaultManager] createFileAtPath:filePath contents:nil attributes:nil];
+            }
+            
+            BOOL sucess =   [resumeData writeToFile:filePath options:0 error:&error];
+            if(sucess){
+                
+                
+            }
+ 
             
         }else{
             
             
         }
-        
-    }];
+    }
     
-    [_downloadTask resume];
+    
 }
-
 
 @end
